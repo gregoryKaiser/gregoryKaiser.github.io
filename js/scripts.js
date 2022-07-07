f3d0243dbc5c387285b2d792dd2af6d8df773583
Gregory Kaiser <gregoryhkaiser@gmail.com>
2022-07-07 16:25:36 -0400

    videos stop when modal closes + highlight fast robots

--- js/scripts.js
+++ js/scripts.js
@@ -64,6 +64,19 @@
         $(this).removeClass("floating-label-form-group-with-focus");
       });
     });
+
+    //
+    // any modals with embedded <iframe> we can assume need reset
+    // <https://stackoverflow.com/a/52315492>
+    //  https://stackoverflow.com/questions/22613303/youtube-video-still-playing-when-bootstrap-modal-closes
+    //
+    $('body').on('hide.bs.modal', '.modal', function() {
+      const $modal = $(this);
+      // return early if there were no embedded YouTube videos
+      if ($modal.find('iframe').length === 0) return;
+      const html = $modal.html();
+      $modal.html(html);
+    });
   
   })(jQuery); // End of use strict
   
